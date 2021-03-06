51d4618ca2f3e1793f44b339ab8740a591b3e717
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-03-06 14:23:29 -0500

    API is down - unable to see page; receiving an error about the DOM

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,36 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export function App() {
-const [currencyRates, setCurrencyRates] = useState({rates: []})
+  const [currencyRates, setCurrencyRates] = useState({ rates: [] })
 
-const (currentCurrency, setCurrentCurrency) = useState(1)
+  const [amount, setAmount] = useState(1)
 
+  useEffect(async function () {
+    const response = await axios.get(
+      'https://api.ratesapi.io/api/latest?base=USD'
+    )
+    setCurrencyRates(response.data)
+  }, [])
 
+  const currencyChoices = Object.entries(currencyRates.rates).map(
+    ([currencyCode, currencyDetails]) => {
+      return (
+        <option key={currencyCode}>
+          {currencyCode}: {(currencyDetails * amount).toFixed(2)}
+        </option>
+      )
+    }
+  )
+
+  return (
+    <div>
+      <h1> Latest Currency Converter</h1>
+      <input>
+        type="number" value={amount}
+        onChange={(event) => setAmount(event.target.value)}
+      </input>
+      <h2> Conversion Rate:</h2>
+      <select>{currencyChoices}</select>
+    </div>
+  )
 }
